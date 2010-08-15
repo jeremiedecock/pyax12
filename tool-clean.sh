6e222c744454d93d6ad7a544db7ce0316fdf7feb
Jeremie DECOCK <gremy@tuxfamily.org>
2010-08-15 18:55:51 +0200

    Remove useless commands

--- tool-clean.sh
+++ tool-clean.sh
@@ -2,11 +2,8 @@
 
 find . -name "*.pyc" -exec rm {} \;
 find . -name "*.pyo" -exec rm {} \;
-rm -rf screencast
 rm -rf scripts
 rm -rf build
 rm -rf dist
 rm -rf debian
 rm MANIFEST
-rm -rf pyarm_logs
-rm -rf pyarm_figs
