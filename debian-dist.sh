9c229333691b307ee41d1abb7488957693b7d11b
Jérémie Decock <jd.jdhp@gmail.com>
2015-06-10 22:29:12 +0200

    Update the 'debian-dist.sh' file (package name, email address, dependencies, ...).

--- debian-dist.sh
+++ debian-dist.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-NAME=pydynamixel
+NAME=pyax12
 VERSION=$(cat VERSION)
 DIST_DIR=dist
 
 rm -rf debian
 
 # TODO
-mkdir -p          debian/usr/local/lib/python2.5/dist-packages
-cp -r pydynamixel debian/usr/local/lib/python2.5/dist-packages
-chmod 644         $(find debian/usr/local/lib -type f)
+mkdir -p     debian/usr/local/lib/python3.0/dist-packages
+cp -r pyax12 debian/usr/local/lib/python3.0/dist-packages
+chmod 644    $(find debian/usr/local/lib -type f)
 
 mkdir -p      "debian/usr/share/doc/$NAME/"
 cp COPYING    "debian/usr/share/doc/$NAME/copyright"
@@ -23,9 +23,9 @@ Package: $NAME
 Version: $VERSION
 Section: libs
 Priority: optional
-Maintainer: Jérémie DECOCK <gremy@tuxfamily.org>
+Maintainer: Jérémie DECOCK <jd.jdhp@gmail.com>
 Architecture: all
-Depends: python (>= 2.5), python-serial
+Depends: python (>= 3.0), python3-serial
 Description: A library to control dynamixel AX-12+ servos with python
 EOF
 
