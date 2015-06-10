26d61e6c006fc02f8a1648cf55cfc6a4a7ab5891
Jérémie Decock <jd.jdhp@gmail.com>
2015-06-10 23:56:55 +0200

    Use argparse to setup the 'dynamixel_id', 'baudrate', 'timeout' and 'port' values.

--- examples/led.py
+++ examples/led.py
@@ -5,15 +5,28 @@ import pydynamixel.packet as pk
 import pydynamixel.connection
 import pydynamixel.instruction_packet as ip
 
+import argparse
 import time
 
 def main():
-    serial_connection = pydynamixel.connection.Connection()
 
-    # Switch ON the LED
+    # PARSE OPTIONS
 
-    #instruction_packet = ip.InstructionPacket(_id=pk.BROADCAST_ID, _instruction=ip.WRITE_DATA, _parameters=(pk.LED, 1))
-    instruction_packet = ip.InstructionPacket(_id=0x02, _instruction=ip.WRITE_DATA, _parameters=(pk.LED, 1))
+    parser = argparse.ArgumentParser(description='A PyAX-12 demo.')
+
+    parser.add_argument("--dynamixel_id", "-i",  help="The unique ID of a Dynamixel unit to work with (254 is a broadcasting ID)", metavar="INTEGER", type=int, default=pk.BROADCAST_ID)
+    parser.add_argument("--baudrate", "-b",  help="The baudrate speed (e.g. 57600)", metavar="INTEGER", type=int, default=57600)
+    parser.add_argument("--timeout", "-t",  help="The timeout value for the connection", metavar="FLOAT", type=float, default=0.1)
+    parser.add_argument("--port", "-p",  help="The serial device to connect with (e.g. '/dev/ttyUSB0' for Unix users)", metavar="STRING", default="/dev/ttyUSB0")
+    args = parser.parse_args()
+
+    # CONNECT TO THE SERIAL PORT
+
+    serial_connection = pydynamixel.connection.Connection(_port=args.port, _baudrate=args.baudrate, _timeout=args.timeout)
+
+    # SWITCH ON THE LED
+
+    instruction_packet = ip.InstructionPacket(_id=args.dynamixel_id, _instruction=ip.WRITE_DATA, _parameters=(pk.LED, 1))
     print('> ', instruction_packet.to_printable_string())
 
     status_packet = serial_connection.send(instruction_packet)
@@ -21,13 +34,13 @@ def main():
     if status_packet is not None:
         print('< ', status_packet.to_printable_string())
 
-    # Wait 2 seconds
+    # WAIT 2 SECONDS
 
     time.sleep(2)
 
-    # Switch OFF the LED
+    # SWITCH OFF THE LED
 
-    instruction_packet = ip.InstructionPacket(_id=pk.BROADCAST_ID, _instruction=ip.WRITE_DATA, _parameters=(pk.LED, 0))
+    instruction_packet = ip.InstructionPacket(_id=args.dynamixel_id, _instruction=ip.WRITE_DATA, _parameters=(pk.LED, 0))
     print('> ', instruction_packet.to_printable_string())
 
     status_packet = serial_connection.send(instruction_packet)
