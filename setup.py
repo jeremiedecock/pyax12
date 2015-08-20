19f099f1d8aefcdc4301e08616d4762e6f7328f5
Jérémie Decock <jd.jdhp@gmail.com>
2015-08-20 18:13:28 +0200

    Fix and shorten the 'setup.py' file.

--- setup.py
+++ setup.py
@@ -26,7 +26,6 @@
 # THE SOFTWARE.
 
 from distutils.core import setup
-import subprocess
 import os
 import shutil
 
@@ -40,65 +39,15 @@ CLASSIFIERS = ['Development Status :: 4 - Beta',
 
 PACKAGES = ['pyax12']
 
-README_FILE = open('README.md', 'r')
+README_FILE = 'README.rst'
 
+def get_long_description():
+    with open(README_FILE, 'r') as fd:
+        desc = fd.read()
+    return desc
 
 def get_version():
-    """
-    Get the version from git or from the VERSION file
-
-    If we're in a git repository, uses the output of ``git describe`` as
-    the version, and update the ``VERSION`` file.
-    Otherwise, read the version from the ``VERSION`` file
-
-    Much inspire from this post:
-    http://dcreager.net/2010/02/10/setuptools-git-version-numbers/
-    """
-
-    # TODO : READ THE VALUE RETURNED BY GIT DESCRIBE : IF NOT EQUALS 0,
-    #        PRINT AN ERROR MESSAGE AND SUGGEST TO LOOK IF ANY TAGS EXISTS.
-    #        Afficher un message pour expliciter les erreurs :
-    #        - cas où la commande git n'existe pas
-    #        - cas où le répertoire courrant n'est pas un référentiel git
-    #        - cas où il n'y a pas de tags dans le référentiel git
-    def get_version_from_git():
-        """Returns the version as defined by ``git describe``, or None."""
-        try:
-            p = subprocess.Popen(['git', 'describe'],
-                                 stdout=subprocess.PIPE,
-                                 stderr=subprocess.PIPE)
-            p.stderr.close()
-            line = p.stdout.readlines()[0].strip()
-            assert line.startswith('v')
-            return line[1:] #remove the leading 'v'
-        except OSError:
-            return None
-        except IndexError:
-            return None
-
-    def get_version_from_file():
-        """Returns the version as defined in the ``VERSION`` file."""
-        f = open('VERSION', 'r')
-        try:
-            version = f.readlines()[0].strip()
-        except OSError:
-            return None
-        finally:
-            f.close()
-        return version
-
-    def update_version_file(version):
-        """Update, if necessary, the ``VERSION`` file."""
-        if version != get_version_from_file():
-            f = open('VERSION', 'w')
-            f.write(version + '\n')
-            f.close()
-
-    version = get_version_from_git()
-    if version:
-        update_version_file(version)
-    else:
-        version = get_version_from_file()
+    version = __import__('pyax12').__version__
     return version
 
 
@@ -125,7 +74,7 @@ setup(author='Jeremie DECOCK',
       classifiers=CLASSIFIERS,
       description='A library to control dynamixel AX-12+ servos with python',
       license='MIT license',
-      long_description=README_FILE.read(),
+      long_description=get_long_description(),
       maintainer='Jeremie DECOCK',
       maintainer_email='jd.jdhp@gmail.com',
       name='pyax12',
@@ -135,5 +84,3 @@ setup(author='Jeremie DECOCK',
       scripts=SCRIPTS.values(),
       url='http://www.jdhp.org/',
       version=get_version())
-
-README_FILE.close()
