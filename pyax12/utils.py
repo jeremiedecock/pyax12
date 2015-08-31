61039dedc67973eff959be9ca8c2631b26aab9c9
Jérémie Decock <jd.jdhp@gmail.com>
2015-08-31 13:19:07 +0200

    Misc updates.

--- pyax12/utils.py
+++ pyax12/utils.py
@@ -135,11 +135,15 @@ def dxl_angle_to_degrees(dxl_angle):
 
     :param int dxl_angle: an angle defined according to the Dynamixel internal
         notation, i.e. in the range (0, 1023) where:
+
         - 0 is a 150° clockwise angle;
         - 1023 is a 150° counter clockwise angle.
+
     :return: an angle defined in degrees in the range (-150.0°, +150.0°) where:
+
         - -150.0 is a 150° clockwise angle;
         - +150.0 is a 150° counter clockwise angle.
+
     :rtype: float.
     """
     angle_degrees = round(dxl_angle / 1023. * 300. - 150.0, 1)
@@ -157,12 +161,16 @@ def degrees_to_dxl_angle(angle_degrees):
 
     :param float angle_degrees: an angle defined in degrees the range
         (-150.0°, +150.0°) where:
+
         - -150.0 is a 150° clockwise angle;
         - +150.0 is a 150° counter clockwise angle.
+
     :return: an angle defined according to the Dynamixel internal notation,
         i.e. in the range (0, 1023) where:
+
         - 0 is a 150° clockwise angle;
         - 1023 is a 150° counter clockwise angle.
+
     :rtype: int.
     """
     dxl_angle = math.floor((angle_degrees + 150.0) / 300. * 1023.)
