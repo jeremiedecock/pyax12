b5dc6f665eaf99f8c169d39d043bbd3f4cafade5
Jeremie DECOCK <jd.jdhp@gmail.com>
2017-05-29 17:13:31 +0200

    Add comments.

--- examples/endless_turn.py
+++ examples/endless_turn.py
@@ -32,13 +32,16 @@ A PyAX-12 demo.
 from pyax12.connection import Connection
 from pyax12.argparse_default import common_argument_parser
 import pyax12.packet as pk
-from pyax12 import utils
 
 import time
 
 def main():
     """
-    A PyAX-12 demo.
+    This is an *endless turn mode* demo.
+
+    If both values for the *CW angle limit* and *CCW angle limit* are set to 0,
+    an *endless turn mode* can be implemented by setting the *goal speed*.
+    This feature can be used for implementing a continuously rotating wheel.
     """
 
     # Parse options
@@ -53,20 +56,24 @@ def main():
 
     dynamixel_id = args.dynamixel_id
 
-    ###
-    
+    # Set the "wheel mode"
     serial_connection.set_cw_angle_limit(dynamixel_id, 0, degrees=False)
     serial_connection.set_ccw_angle_limit(dynamixel_id, 0, degrees=False)
+
+    # Activate the actuator (speed=512)
     serial_connection.set_speed(dynamixel_id, 512)
 
-    time.sleep(5)    # Wait 5 seconds
+    # Lets the actuator turn 5 seconds
+    time.sleep(5)
 
+    # Stop the actuator (speed=0)
     serial_connection.set_speed(dynamixel_id, 0)
+
+    # Leave the "wheel mode"
     serial_connection.set_ccw_angle_limit(dynamixel_id, 1023, degrees=False)
 
+    # Go to the initial position (0 degree)
     serial_connection.goto(dynamixel_id, 0, speed=512, degrees=True)
-
-    ###
 
     # Close the serial connection
     serial_connection.close()
